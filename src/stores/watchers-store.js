5d703ba042ada4dbccfeb494605bde12d64b4624
Mikk Kirstein <mikk@toggl.com>
2015-09-03 12:29:44 +0300

    refactored weatchers store

--- src/stores/watchers-store.js
+++ src/stores/watchers-store.js
@@ -2,19 +2,13 @@
 
 const _        = require('lodash');
 const chokidar = require('chokidar');
-const hub      = require('../event-hub');
 
 const constants = {
   deps: require('../constants/dependency-constants'),
   watcher: require('../constants/watcher-constants')
 };
 
-// List of watchers according to their path
-//
-// path: { watcher: <chokidar>, dependency: <dependency> }
-var data = {};
-
-function buildWatcher (dependency) {
+function buildWatcher (hub, dependency) {
   let watcher = chokidar.watch(dependency.path);
   watcher.on('change', function () {
     hub.emit(constants.watcher.DEPENDENCY_FILE_CHANGED, dependency);
@@ -25,43 +19,49 @@ function buildWatcher (dependency) {
   return watcher;
 }
 
-function addMultipleIfNeeded (deps) {
-  _.each(deps, function (dependency) {
-    let path = dependency.path;
-    // That path is already added.
-    if (data[path]) {
-      return;
-    }
-    data[path] = {
-      dependency: dependency,
-      watcher: buildWatcher(dependency)
-    };
-  });
+class DependenciesStore {
+  constructor (hub) {
+    this._hub  = hub;
+    this._data = {};
+    this._registerEvents();
+  }
+
+  _registerEvents () {
+    this._hub.on(constants.deps.MULTIPLE_DEPENDENCY_ADDED, this.addMultipleIfNeeded.bind(this));
+    this._hub.on(constants.deps.MULTIPLE_DEPENDENCY_CHANGED, this.addMultipleIfNeeded.bind(this));
+    this._hub.on(constants.deps.DEPENDENCY_UNWATCH, this.unwatch.bind(this));
+    this._hub.on(constants.deps.MULTIPLE_DEPENDENCY_UNWATCH, this.multipleUnwatch.bind(this));
+  }
+
+  getWatchers () {
+    return this._data;
+  }
+
+  multipleUnwatch (deps) {
+    _.each(deps, this.unwatch, this);
+  }
+
+  unwatch (path) {
+    let dep = this._data[path];
+    // Seems that we do not have a target to unwatch. Oh well.
+    if (!dep) { return; }
+    dep.watcher.close();
+    delete this._data[path];
+  }
+
+  addMultipleIfNeeded (deps) {
+    _.each(deps, function (dependency) {
+      let path = dependency.path;
+      // That path is already added.
+      if (this._data[path]) {
+        return;
+      }
+      this._data[path] = {
+        dependency: dependency,
+        watcher: buildWatcher(this._hub, dependency)
+      };
+    }, this);
+  }
 }
 
-function unwatch (path) {
-  let dep = data[path];
-  // Seems that we do not have a target to unwatch. Oh well.
-  if (!dep) { return; }
-  dep.watcher.close();
-  delete data[path];
-}
-
-function multipleUnwatch (deps) {
-  _.each(deps, unwatch);
-}
-
-exports.getWatchers = function () {
-  return data;
-};
-
-exports.clear = function () {
-  data = {};
-};
-
-exports._registerEvents = function () {
-  hub.on(constants.deps.MULTIPLE_DEPENDENCY_ADDED, addMultipleIfNeeded);
-  hub.on(constants.deps.MULTIPLE_DEPENDENCY_CHANGED, addMultipleIfNeeded);
-  hub.on(constants.deps.DEPENDENCY_UNWATCH, unwatch);
-  hub.on(constants.deps.MULTIPLE_DEPENDENCY_UNWATCH, multipleUnwatch);
-};
+module.exports = DependenciesStore;
