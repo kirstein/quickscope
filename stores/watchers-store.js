4e0ced944e7c567353c48a66cdc65412f1d1c389
Mikk Kirstein <github@kolmas.org>
2016-09-19 20:36:17 +0300

    implemented new linter rules for watchers store

--- stores/watchers-store.js
+++ stores/watchers-store.js
@@ -1,61 +1,97 @@
 'use strict';
 
-const _        = require('lodash');
+const _ = require('lodash');
 const chokidar = require('chokidar');
 
 const constants = {
-  deps: require('../constants/dependency-constants'),
-  watcher: require('../constants/watcher-constants')
+    deps: require('../constants/dependency-constants'),
+    watcher: require('../constants/watcher-constants')
 };
 
-function buildWatcher (hub, dependency) {
-  let watcher = chokidar.watch(dependency.path);
-  watcher.on('change', () => hub.emit(constants.watcher.DEPENDENCY_FILE_CHANGED, dependency));
-  watcher.on('unlink', () => hub.emit(constants.watcher.DEPENDENCY_FILE_UNLINK, dependency));
-  return watcher;
+/**
+ * Builds watcher and links `change` and `unlink` events on that
+ *
+ * @param {Hub} hub hub to emit the given events to
+ * @param {Dependency} dependency dependency to control
+ * @return {Watcher} new build watcher
+ */
+function buildWatcher(hub, dependency) {
+    const watcher = chokidar.watch(dependency.path);
+    watcher.on('change', () => hub.emit(constants.watcher.DEPENDENCY_FILE_CHANGED, dependency));
+    watcher.on('unlink', () => hub.emit(constants.watcher.DEPENDENCY_FILE_UNLINK, dependency));
+    return watcher;
 }
 
 class DependenciesStore {
-  constructor (hub) {
-    this._hub  = hub;
-    this._data = {};
-    this._registerEvents();
-  }
+    /**
+     * @param {Hub} hub target hub to use for event transmitting
+     */
+    constructor(hub) {
+        this._hub = hub;
+        this._data = {};
+        this._registerEvents();
+    }
 
-  _registerEvents () {
-    this._hub.on(constants.deps.MULTIPLE_DEPENDENCY_ADDED, this.addMultipleIfNeeded.bind(this));
-    this._hub.on(constants.deps.MULTIPLE_DEPENDENCY_CHANGED, this.addMultipleIfNeeded.bind(this));
-    this._hub.on(constants.deps.DEPENDENCY_UNWATCH, this.unwatch.bind(this));
-    this._hub.on(constants.deps.MULTIPLE_DEPENDENCY_UNWATCH, this.multipleUnwatch.bind(this));
-  }
+    /**
+     * Register events on target hub
+     */
+    _registerEvents() {
+        this._hub.on(constants.deps.MULTIPLE_DEPENDENCY_ADDED, this.addMultipleIfNeeded.bind(this));
+        this._hub.on(constants.deps.MULTIPLE_DEPENDENCY_CHANGED, this.addMultipleIfNeeded.bind(this));
+        this._hub.on(constants.deps.DEPENDENCY_UNWATCH, this.unwatch.bind(this));
+        this._hub.on(constants.deps.MULTIPLE_DEPENDENCY_UNWATCH, this.multipleUnwatch.bind(this));
+    }
 
-  getWatchers () {
-    return this._data;
-  }
+    /**
+     * Retuns all watchers
+     *
+     * @return {Array.<Watcher>} watchers
+     */
+    getWatchers() {
+        return this._data;
+    }
 
-  multipleUnwatch (deps) {
-    _.each(deps, (dep) => this.unwatch(dep));
-  }
+    /**
+     * Unwatch multiple dependencies
+     *
+     * @param {Array.<Dependency>} deps deps to unwatch
+     */
+    multipleUnwatch(deps) {
+        _.each(deps, (dep) => this.unwatch(dep));
+    }
 
-  unwatch (path) {
-    let dep = this._data[path];
-    // Seems that we do not have a target to unwatch
-    if (!dep) { return; }
-    dep.watcher.close();
-    delete this._data[path];
-  }
+    /**
+     * Unwatch single dependency.
+     * Close the watcher and delete it from watchers list
+     *
+     * @param {string} path watcher path
+     */
+    unwatch(path) {
+        const dep = this._data[path];
+        // Seems that we do not have a target to unwatch
+        if (!dep) { return; }
+        dep.watcher.close();
+        delete this._data[path];
+    }
 
-  addMultipleIfNeeded (deps) {
-    _.each(deps, (dependency) => {
-      let path = dependency.path;
-      // That path is already added.
-      if (this._data[path]) return;
-      this._data[path] = {
-        dependency: dependency,
-        watcher: buildWatcher(this._hub, dependency)
-      };
-    });
-  }
+    /**
+     * Add multiple watchers if needed.
+     * Will go through the watchers list and see if the given target is already watched.
+     * If it is not the will watch that
+     *
+     * @param {Array.<Dependency>} deps dependencies to add
+     */
+    addMultipleIfNeeded(deps) {
+        _.each(deps, (dependency) => {
+            const path = dependency.path;
+            // That path is already added.
+            if (this._data[path]) return;
+            this._data[path] = {
+                dependency: dependency,
+                watcher: buildWatcher(this._hub, dependency)
+            };
+        });
+    }
 }
 
 module.exports = DependenciesStore;
