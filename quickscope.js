ade15503d255396f7fd2a3dcc3c8ea2f1ca15da9
Mikk Kirstein <github@kolmas.org>
2016-09-19 20:07:48 +0300

    added better linter rules for quickscope main

--- quickscope.js
+++ quickscope.js
@@ -1,85 +1,131 @@
 'use strict';
 
-const events   = require('events');
-const path     = require('path');
-const _        = require('lodash');
+const events = require('events');
+const path = require('path');
+const _ = require('lodash');
 const chokidar = require('chokidar');
 
 const constants = {
-  watcher    : require('./constants/watcher-constants'),
-  target     : require('./constants/target-constants'),
-  deps       : require('./constants/dependency-constants'),
-  quickscope : require('./constants/quickscope-constants')
+    watcher: require('./constants/watcher-constants'),
+    target: require('./constants/target-constants'),
+    deps: require('./constants/dependency-constants'),
+    quickscope: require('./constants/quickscope-constants')
 };
 
 const DependenciesStore = require('./stores/dependency-store');
-const WatchersStore     = require('./stores/watchers-store');
+const WatchersStore = require('./stores/watchers-store');
 
 const DEFAULT_OPTS = {
-  persistent: true,
-  alwaysStat: false,
-  followSymlinks: true,
-  cwd: process.cwd()
+    persistent: true,
+    alwaysStat: false,
+    followSymlinks: true,
+    cwd: process.cwd()
 };
 
-function buildWatcher (glob, opts) {
-  return chokidar.watch(glob, opts);
+/**
+ * @param {string} glob glob to watch
+ * @param {Object} opts additional params
+ * @return {Watcher}
+ */
+function buildWatcher(glob, opts) {
+    return chokidar.watch(glob, opts);
 }
 
 class Quickscope extends events.EventEmitter {
-  constructor (glob, opts) {
-    if (!glob) { throw new Error('No glob defined'); }
-    opts = _.assign({}, DEFAULT_OPTS, opts);
-    super();
-    this.cwd      = opts.cwd;
-    this._hub     = new events.EventEmitter();
-    this._targets = [];
-    this._initStores();
-    this._addHubListeners();
-    this._addWatcherListeners(buildWatcher(glob, opts));
-  }
 
-  _initStores () {
-    this._depStore      = new DependenciesStore(this._hub);
-    this._watchersStore = new WatchersStore(this._hub);
-  }
+    /**
+     * @param {string} glob glob
+     * @param {object} opts additional params
+     */
+    constructor(glob, opts) {
+        if (!glob) { throw new Error('No glob defined'); }
+        opts = _.assign({}, DEFAULT_OPTS, opts);
+        super();
+        this.cwd = opts.cwd;
+        this._hub = new events.EventEmitter();
+        this._targets = [];
+        this._initStores();
+        this._addHubListeners();
+        this._addWatcherListeners(buildWatcher(glob, opts));
+    }
 
-  _addWatcherListeners (watcher) {
-    watcher.on('add', this.addTarget.bind(this));
-    watcher.on('ready', this._triggerReady.bind(this));
-    watcher.on('unlink', this.unlinkTarget.bind(this));
-  }
+    /**
+     * Initiates stores
+     */
+    _initStores() {
+        this._depStore = new DependenciesStore(this._hub);
+        this._watchersStore = new WatchersStore(this._hub);
+    }
 
-  _addHubListeners () {
-    this._hub.on(constants.watcher.DEPENDENCY_FILE_CHANGED, this.changeDependency.bind(this));
-    this._hub.on(constants.deps.MULTIPLE_DENENDENCY_DIRTY, this.changeDependency.bind(this));
-  }
+    /**
+     * @param {Watcher} watcher watcher to bind events on
+     */
+    _addWatcherListeners(watcher) {
+        watcher.on('add', this.addTarget.bind(this));
+        watcher.on('ready', this._triggerReady.bind(this));
+        watcher.on('unlink', this.unlinkTarget.bind(this));
+    }
 
-  _triggerReady () {
-    this.emit(constants.quickscope.QUICKSCOPE_READY, this._targets);
-  }
+    /**
+     * Adds hubs listeners.
+     *
+     * Hub is the event bus between stores and the quickscope itself
+     */
+    _addHubListeners() {
+        this._hub.on(constants.watcher.DEPENDENCY_FILE_CHANGED, this.changeDependency.bind(this));
+        this._hub.on(constants.deps.MULTIPLE_DENENDENCY_DIRTY, this.changeDependency.bind(this));
+    }
 
-  addTarget (target) {
-    if (!target) throw new Error('No target defined');
-    this._targets.push(target);
-    this._hub.emit(constants.target.TARGET_ADDED, {
-      path: target,
-      cwd : this.cwd
-    });
-    this.emit(constants.quickscope.QUICKSCOPE_TARGET_ADD, target);
-  }
+    /**
+     * Trigger ready event on itself.
+     *
+     * This given event is emitted when the quickscope is in the following state:
+     *  - parsed AST and found all dependencies
+     *  - initiated watchers on all necessary files
+     *  - waiting for file changes
+     */
+    _triggerReady() {
+        this.emit(constants.quickscope.QUICKSCOPE_READY, this._targets);
+    }
 
-  unlinkTarget (target) {
-    if (!target) throw new Error('No target defined');
-    this._targets = _.without(this._targets, target);
-    this._hub.emit(constants.target.TARGET_REMOVED, path.join(this.cwd, target));
-    this.emit(constants.quickscope.QUICKSCOPE_TARGET_UNLINK, target);
-  }
+    /**
+     * Adds target a new entry point to the mix
+     * Triggers `QUICKSCOPE_TARGET_ADD`.
+     *
+     * @param {string} target target to add
+     */
+    addTarget(target) {
+        if (!target) throw new Error('No target defined');
+        this._targets.push(target);
+        this._hub.emit(constants.target.TARGET_ADDED, {
+            path: target,
+            cwd: this.cwd
+        });
+        this.emit(constants.quickscope.QUICKSCOPE_TARGET_ADD, target);
+    }
 
-  changeDependency (dep) {
-    if (!dep) throw new Error('Dependency not defined');
-    this.emit(constants.quickscope.QUICKSCOPE_DEP_CHANGE, [].concat(dep));
-  }
+    /**
+     * Unlink target file
+     * Triggers `QUICKSCOPE_TARGET_UNLINK` event
+     *
+     * @param {string} target target to unlink
+     */
+    unlinkTarget(target) {
+        if (!target) throw new Error('No target defined');
+        this._targets = _.without(this._targets, target);
+        this._hub.emit(constants.target.TARGET_REMOVED, path.join(this.cwd, target));
+        this.emit(constants.quickscope.QUICKSCOPE_TARGET_UNLINK, target);
+    }
+
+    /**
+     * Will be triggered when the one of the dependency has been changed
+     *
+     * @param {Dependency} dep target dependency
+     */
+    changeDependency(dep) {
+        if (!dep) throw new Error('Dependency not defined');
+        this.emit(constants.quickscope.QUICKSCOPE_DEP_CHANGE, [].concat(dep));
+    }
 }
 
 Quickscope.DEFAULT_OPTS = DEFAULT_OPTS;
